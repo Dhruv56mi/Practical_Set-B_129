c22c9e05f2d6040e8be1948ad6fc96b0243f05a7
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-22 20:59:22 +0530

    Updated practical file - Chelsi

--- practical_3.py
+++ practical_3.py
@@ -8,3 +8,21 @@ for i in range(1, rows+1):
         if num > 9:
             num = 1
     print()
+# Practical A4 code 
+num = 1
+
+for i in range(5, 3, -1):   # 5 and 4
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
+# Reset number
+num = 1
+
+# Next three rows
+for i in range(3, 0, -1):   # 3, 2, 1
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
